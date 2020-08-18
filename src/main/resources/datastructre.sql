ead4ef41a3bb1b4d3050757c107319a0d4bfda99
rubo92i <ruben.manukyan@appish.co.uk>
2020-08-18 21:22:11 +0400

    init

---
+++ src/main/resources/datastructre.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `oauth_access_token` (
+  `token_id` VARCHAR(256) DEFAULT NULL,
+  `token` BLOB,
+  `authentication_id` VARCHAR(256) DEFAULT NULL,
+  `user_name` VARCHAR(256) DEFAULT NULL,
+  `client_id` VARCHAR(256) DEFAULT NULL,
+  `authentication` BLOB,
+  `refresh_token` VARCHAR(256) DEFAULT NULL
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `oauth_refresh_token` (
+  `token_id` VARCHAR(256) DEFAULT NULL,
+  `token` BLOB,
+  `authentication` BLOB
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
